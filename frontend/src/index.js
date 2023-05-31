2f0232699ae550de8a8cede697a8674d1d64f78e
rokasj <rokas@jerio.me>
2023-05-31 04:50:04 -0400

    Add csrfFetch and restoreSession

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+import { restoreSession, csrfFetch } from './store/csrf';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+};
+
+const initializeApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+restoreSession().then(initializeApp)
