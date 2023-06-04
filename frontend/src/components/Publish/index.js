c05d7c1d4da81f57b9d438041efb1376ef36ac7f
rokasj <rokas@jerio.me>
2023-06-04 12:12:52 -0400

    Remove user ability to delete last line

--- frontend/src/components/Publish/index.js
+++ frontend/src/components/Publish/index.js
@@ -12,85 +12,71 @@ const Publish = props => {
         return node;
     }
 
-    const handleDefaultTextAndFocus = () => {
+    const handleDefaultTextAndFocusAndDeleteLastLine = () => {
         const content = document.body.querySelector('.publish-content');
-        
+
+        document.addEventListener('selectionchange', e => {
+            const selection = window.getSelection();
+            const selectedNode = selection.anchorNode;
+            const parentDivElement = getDivParent(selectedNode);
+            const parentDivClassList = parentDivElement.classList;
+            const previousFocused = document.body.querySelector('.focused');
+
+            if (parentDivElement.classList.contains('input-div')) {
+                if (previousFocused) {
+                    previousFocused.classList.remove('focused');
+                }
+
+                parentDivClassList.add('focused');
+            }
+
+            if (parentDivClassList.contains('unmodified')) {
+                const range = document.createRange();
+                range.setStart(parentDivElement, 0);
+                range.collapse(true);
+                selection.removeAllRanges();
+                selection.addRange(range);
+            };
+        });
+
+
         content.addEventListener('keydown', e => {
             const selection = window.getSelection();
             const selectedNode = selection.anchorNode;
-            const parentElement = getDivParent(selectedNode);
-            const selectedClassList = parentElement.classList;
-            const previousFocused = document.body.querySelector('.focused');
-
-            if (previousFocused) {
-                previousFocused.classList.remove('focused');
-            }
-
-            parentElement.classList.add('focused');
+            const parentDivElement = getDivParent(selectedNode);
+            const parentDivClassList = parentDivElement.classList;
 
             if (!['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(e.key) &&
-                selectedClassList.contains('unmodified')) {
-                     {
-                        selectedNode.textContent = '';
-                        selectedClassList.remove('unmodified');
-                    };
-                }
+                parentDivClassList.contains('unmodified')) {
+                {
+                    selectedNode.textContent = '';
+                    parentDivClassList.remove('unmodified');
+                };
+            };
 
-            // if (selectedNode.textContent === '' &&  //ATTEMPT AT PREVENTING USER DELETION OF LAST LINE
-            //     e.key === 'Backspace' &&
-            //     selectedNode.classList.contains('input-div') &&
-            //     parentElement.children.length > 1
-            //     ) {
-            //         e.preventDefault(); //THIS DOESNT DO ANYTHING
-            //         selectedNode.remove();
-            // };
+            if (selectedNode.textContent === '' &&
+                e.key === 'Backspace' &&
+                parentDivClassList.contains('input-div') &&
+                content.children.length <= 1
+            ) {
+                e.preventDefault();
+                const newDiv = document.createElement('div');
+                newDiv.classList.add('input-div');
+                newDiv.classList.add('focused');
+                content.appendChild(newDiv);
+                content.removeChild(content.firstChild);
+
+                const range = document.createRange();
+                range.setStart(newDiv, 0);
+                range.collapse(true);
+                selection.removeAllRanges();
+                selection.addRange(range);
+            };
         });
-
-        content.addEventListener('keyup', e => {
-            const selection = window.getSelection();
-            const selectedNode = selection.anchorNode;
-            const parentElement = getDivParent(selectedNode)
-            const previousFocused = document.body.querySelector('.focused');
-
-            if (previousFocused) {
-                previousFocused.classList.remove('focused');
-            }
-
-            parentElement.classList.add('focused');
-        });
-
-        content.addEventListener('click', e => {
-            const selection = window.getSelection()
-            const selectedNode = selection.anchorNode;
-            const parentElement = getDivParent(selectedNode)
-            const selectedClassList = parentElement.classList;
-            const previousFocused = document.body.querySelector('.focused');
-            
-            if (previousFocused) {
-                previousFocused.classList.remove('focused');
-            }
-
-            selectedClassList.add('focused');
-        });
-
-        // content.addEventListener('click', e => {  //ATTEMPT AT MAKING CURSOR START AT BEGINNING OF DEFAULT TEXT LINES
-        //     const selection = window.getSelection()
-        //     const selectedNode = selection.anchorNode;
-        //     const parentElement = selectedNode.parentElement;
-        //     const selectedClassList = parentElement.classList;
-
-        //     if (selectedClassList.contains('unmodified')) {
-        //         const range = document.createRange();
-        //         range.setStart(parentElement, 0);
-        //         range.collapse(true);
-        //         selection.removeAllRanges();
-        //         selection.addRange(range);
-        //     };
-        // });
-    };
+    }
 
     useEffect(() => {
-        handleDefaultTextAndFocus();
+        handleDefaultTextAndFocusAndDeleteLastLine();
     }, []);
 
     return (
