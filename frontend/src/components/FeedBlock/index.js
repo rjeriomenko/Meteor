c8405b48f5ead3aa24dd6d51312a5bf3885f675d
rokasj <rokas@jerio.me>
2023-06-07 15:13:35 -0400

    Format FeedBlock

--- frontend/src/components/FeedBlock/index.js
+++ frontend/src/components/FeedBlock/index.js
@@ -1,22 +1,37 @@
 import './FeedBlock.css';
 import Loading from '../Loading/index';
+import logo from '../../logo.png';
 import { getTale, fetchTale } from '../../store/talesReducer';
 import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 const FeedBlock = ({ tale, user }) => {
-    console.log(user)
     const dispatch = useDispatch();
 
     const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.currentUser));
     const [loading, setLoading] = useState(false);
     const [scrollToReload, setscrollToReload] = useState(300);
 
-    // const renderContent = contentString => {
-    //     const content = document.body.querySelector('.show-content')
-    //     content.innerHTML = contentString;
-    // }
+    const getAuthorPicture = () => {
+        return (
+            <img src={logo} alt='Default Author Picture' className='feed-block-item author-picture' />
+        )
+    }
+
+    const getIntro = () => {
+        const intro = tale.content.slice(0, 150) + '...';
+
+        return(
+            <div className='feed-block-item feed-block-intro'>{intro}</div>
+        )
+    }
+
+    const getThumbnail = () => {
+        return (
+            <img src={logo} alt='Default Thumbnail' className='thumbnail' />
+        )
+    }
 
     //Handles Page Loading
     // useEffect(() => {
@@ -38,11 +53,22 @@ const FeedBlock = ({ tale, user }) => {
         <Loading />
     )} else return (
         <div className='feed-block'>
-            <div className='feed-block-author'>
-                {user.fullName}
-            </div>
-            <div className='feed-block-title'>
-                {tale.title}
+            <div className='feed-block-content'>
+                <div className='feed-block-content-text'>
+                    <div className='feed-block-author'>
+                        {getAuthorPicture()}
+                        <div className='feed-block-item author-fullname'>{user.fullName}</div>
+                        <div className='feed-block-item publish-date'>.PUBDATE</div>
+                    </div>
+                        <div className='feed-block-item feed-block-title'>
+                            {tale.title}
+                        </div>
+                        {getIntro()}
+                        <div className='feed-block-item feed-block-constellation'>
+                            TAGDRAMA
+                        </div>
+                </div>
+                    {getThumbnail()}
             </div>
         </div>
     );
