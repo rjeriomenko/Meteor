b609233044e188dfe8eee9a775486b11164e0825
rokasj <rokas@jerio.me>
2023-06-09 09:46:49 -0400

    Remove stars to allow Render deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,22 +72,13 @@ ApplicationRecord.transaction do
         publish_time: "2023-05-25T05:27:11-04:00"
     )
 
-    puts "Creating Stars..."
-    200.times do
-        Star.create!(
-            user_id: rand(1..40),
-            tale_id: rand(1..6)
-        )
-    end
-
-    # puts "Creating Comets..."
+    # puts "Creating Stars..."
     # 200.times do
     #     Star.create!(
     #         user_id: rand(1..40),
-    #         tale_id: rand(1..6),
+    #         tale_id: rand(1..7)
     #     )
     # end
-
 
     puts "Done Seeding"
 end
