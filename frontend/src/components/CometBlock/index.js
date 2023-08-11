ffebfbd3e4a57405bb33454c9ae7b2fba6aa3b4a
rokasj <rokas@jerio.me>
2023-08-11 11:28:17 -0400

    add working comet delete

--- frontend/src/components/CometBlock/index.js
+++ frontend/src/components/CometBlock/index.js
@@ -1,11 +1,14 @@
 import './CometBlock.css';
 import Loading from '../Loading/index';
 import invertedLogo from '../../inverted-logo.png';
+import { deleteComet, fetchComets } from '../../store/cometsReducer';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-const CometBlock = ({ comet, author, currentUser }) => {
+const CometBlock = ({ comet, author, currentUser, taleId }) => {
+    const dispatch = useDispatch();
+
     const getAuthorPicture = () => {
         return (
             <img src={invertedLogo} alt='Default Author Picture' className='feed-block-item author-picture' />
@@ -18,14 +21,23 @@ const CometBlock = ({ comet, author, currentUser }) => {
         )
     }
 
+    const handleDelete = e => {
+        e.target.classList.add('comet-deleting');
+        
+        dispatch(deleteComet(comet.id))
+            .then(() => {
+                dispatch(fetchComets(taleId));
+            })
+    }
+
     const renderCometCrud = () => {
         if (currentUser?.id === comet.userId) {
             return (
                 <>
-                    <div className='comet-feed-block-button' id={`comet-edit-${comet.id}`} >
+                    <div className='comet-feed-block-button' id={`comet-edit-${comet.id}`}>
                         Edit
                     </div>
-                    <div className='comet-feed-block-button' id={`comet-delete-${comet.id}`} >
+                    <div className='comet-feed-block-button' id={`comet-delete-${comet.id}`} onClick={handleDelete}>
                         Delete
                     </div>
                 </>
